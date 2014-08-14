1d884dd646d9554f94ef9d8bed9f0617f10d0c99
Manoj <pmontu@gmail.com>
2014-08-14 17:17:11 +0530

    custom filter with parameter and $filter date

--- js/ALOG.js
+++ js/ALOG.js
@@ -2,9 +2,10 @@
 
 var app = angular.module("alogApp",[]);
 
-app.filter('datehour', function(){
+app.filter('datehour', function($filter){
 	return function(items, date, hour){
 		output = [];
+		date = $filter('date')(date,"yyyy-MM-dd");
 		angular.forEach(items,function(activity){
 			if(activity.date == date && activity.hour == hour)
 				output.push(activity);
@@ -32,26 +33,21 @@ app.controller("viewGridController", function($scope, viewGridFactory) {
 			$scope.data = data;
 			console.log(data);
 		});
+
+		var i=0;
+		while(i<5){
+			var d = new Date($scope.dateStart);
+			d.setDate(d.getDate()+i);
+			$scope.dates[i] = d;
+			i++;
+		}
+
 	}
 
 	//
 	// SELECT DATE TO LOAD ACTIVITIES
 	//
 	$scope.change = function(){
-
-		//
-		//	CHANGE DATES & CALL INIT WHICH CALLS GETACTIVITIES AND UPDATES GRID
-		//
-
-		var i=0;
-		while(i<5){
-
-			var d = new Date($scope.dateStart);
-			d.setDate(d.getDate()+i);
-			$scope.dates[i] = d;
-
-			i++;
-		}
 
 		init();
 	};
