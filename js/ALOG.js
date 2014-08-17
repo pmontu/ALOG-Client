33bdb8a6f07d073d3d1c440f6a9e1d300bbeca4c
Manoj <pmontu@gmail.com>
2014-08-17 13:46:09 +0530

    plus sign, preparation of activities

--- js/ALOG.js
+++ js/ALOG.js
@@ -27,7 +27,9 @@ app.filter('datehour', function($filter){
 
 app.controller("viewGridController", function($scope, viewGridFactory, $filter) {
 
+	//	HOURS TO FILTER
 	$scope.hours= [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22];
+	//	NUMBER OF DAYS TO DISPLAY
 	$scope.daysnumber = 7;
 
 	//	MIN MAX DATES FOR DATE PICKER
@@ -38,12 +40,20 @@ app.controller("viewGridController", function($scope, viewGridFactory, $filter) 
 		});
 	}
 
+	//	RUNS ON PAGE START
 	setDateRange();
+
+	//	SELECT DATE TO LOAD ACTIVITIES
+	$scope.change = function(){
+
+		init();
+	};
 
 	//	LOADS ACTIVITIES OF NUMBER OF DAYS FROM START DATE ON SCREEN
 	//	MUST FILL DATES TOO SO THAT BOTH ARE USED BY PRESENTATION
 	function init(){
 		
+		//	ACTIVITIES
 		var d = new Date($scope.dateStart);
 		var year = d.getFullYear();
 		var month = d.getMonth() + 1;
@@ -51,13 +61,52 @@ app.controller("viewGridController", function($scope, viewGridFactory, $filter) 
 
 		viewGridFactory.getActivities(year,month,day,$scope.daysnumber).success(function(data){
 			$scope.data = data;
+			$scope.activities = getPreparedActivities($scope.data, $scope.hours,$scope.dates,$filter('datehour'));
 		});
 
+		//	DATES TO FILTER
 		$scope.dates = getDates($scope.dateStart,$scope.daysnumber);
-
+		
+		//	PAGER	
 		$scope.isPreviousAllowed = getIsPreviousAllowed($scope.datemin,$scope.dateStart);
 		$scope.isNextAllowed = getIsNextAllowed($scope.datemax,$scope.dateStart,$scope.daysnumber);
 
+	}
+
+	function getDates(startdate,daysnumber){
+		dates = [];
+		var i=0;
+		while(i<daysnumber){
+			var d = new Date(startdate);
+			d.setDate(d.getDate()+i);
+			dates.push(d);
+			i++;
+		}
+		return dates;
+	}
+
+	function getPreparedActivities(data, hours, dates, datehourfilter){
+		var activities=[];
+		angular.forEach(hours, function(h){
+			var a = []
+			angular.forEach(dates, function(d){
+				var acell = datehourfilter(data,d,h);
+				c = [];
+				angular.forEach(acell, function(b){
+					c.push(b);
+				});
+				var isEmpty=false;
+				if(c.length==0){
+					isEmpty=true;
+				}
+				a.push({
+					"isEmpty":isEmpty,
+					"cells":c
+				});
+			});
+			activities.push(a);
+		});
+		return activities;
 	}
 
 	function getIsPreviousAllowed(mindate,startdate){
@@ -77,42 +126,26 @@ app.controller("viewGridController", function($scope, viewGridFactory, $filter) 
 		return false;
 	}
 
-	function getDates(startdate,daysnumber){
-		dates = [];
-		var i=0;
-		while(i<daysnumber){
-			var d = new Date(startdate);
-			d.setDate(d.getDate()+i);
-			dates.push(d);
-			i++;
-		}
-		return dates;
-	}
-
-	//	SELECT DATE TO LOAD ACTIVITIES
-	$scope.change = function(){
-
-		init();
-	};
-
 
 });
 
 
 app.factory('viewGridFactory', function($http){
 	var factory = {};
+
+	var webServerDomainName = "192.168.0.2:12345";
 	
 	// GET NUMBER OF DAYS OF ACTIVITIES
 	factory.getActivities = function(year,month,day,daysnumber) {
 		return $http.get(
-				'http://127.0.0.1:12345/activity/activities/'+
+				'http://'+webServerDomainName+'/activity/activities/'+
 				year+'/'+month+'/'+day+'/'+daysnumber+'/rows.json'
 			);
 	};
 
 	//	GET DATE RANGE
 	factory.getDateRange = function(){
-		return $http.get('http://127.0.0.1:12345/activity/activities/daterange.json');
+		return $http.get('http://'+webServerDomainName+'/activity/activities/daterange.json');
 	};
 	return factory;
 });
